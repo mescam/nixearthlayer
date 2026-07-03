89851d1cc45dc0ab071c70d43a18e1c5de309ba0
mescam <480002+mescam@users.noreply.github.com>
2026-07-03 22:48:51 +0000

    Update xearthlayer to 0.4.6

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
           inherit system overlays;
         };
 
-        version = "0.3.1";
+        version = "0.4.6";
         rev = "v${version}";
 
         rustPlatform = pkgs.makeRustPlatform {
@@ -34,7 +34,7 @@
             owner = "samsoir";
             repo = "xearthlayer";
             rev = rev;
-            hash = "sha256-1GeMPrn5z11t+us4pArNERjauaHEPoDiW6qBRRLYi0M=";
+            hash = "sha256-Ur5SEigBqglx+jE4OwtWENWmb1hwdvr7RPrQ530hdKo=";
           };
 
           cargoLock = {
